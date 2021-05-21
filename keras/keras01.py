70504fece9ef43ae9c3c310ce66838d09958ea77
rohsik2 <rohsik2@gmail.com>
2021-05-21 18:54:22 +0900

    first tensorflow stucy commit

---
+++ keras/keras01.py
@@ -0,0 +1,26 @@
+import numpy as np
+import tensorflow as tf
+
+#1. Data
+x = np.array([1,2,3])
+y = np.array([1,2,3])
+
+#2. Modeling
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense
+
+model = Sequential()
+model.add(Dense(3000, input_dim=1))  # input's dimension is one == one input node
+model.add(Dense(4000))
+model.add(Dense(2000))
+model.add(Dense(1))
+
+#3. Compile, Training
+model.compile(loss='mse', optimizer='adam')
+model.fit(x=x, y=y, epochs=500, batch_size=1)
+
+#4. evaluate, Prediction
+loss = model.evaluate(x,y, batch_size=1)
+results = model.predict([4])
+print("loss :", loss)
+print("results :", results)
