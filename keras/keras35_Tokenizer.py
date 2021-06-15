e916ce0a46c05fc07e589888e88eaf655cb95816
unknown <rohsik2@gmail.com>
2021-06-15 19:14:28 +0900

    tokenizer

---
+++ keras/keras35_Tokenizer.py
@@ -0,0 +1,19 @@
+from tensorflow.keras.preprocessing.text import Tokenizer
+
+text = "I ate really really delcious food"
+
+token = Tokenizer()
+token.fit_on_texts([text])
+
+print(token.word_index)
+
+x = token.texts_to_sequences([text])
+print(x)
+# Changing Data to Dictionanry and make words to numeric data.
+
+from tensorflow.keras.utils import to_categorical
+word_size = len(token.word_index)
+print(word_size)
+x = to_categorical(x)
+print(x)
+print(x.shape)
